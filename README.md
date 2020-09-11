2a7eb65f563735f5784d6453e0e734ebf3229392
tristanlatr <tris.la.tr@gmail.com>
2020-09-11 18:58:44 -0400

    Works successfully

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# esm_api_docs
-This program create a PDF from your McAfee SIEM API documentation
+# McAfee SIEM API documentation to PDF
+
+Create a clickable PDF of the McAfee SIEM API documentation.  
+
+Install
+
+Clone this repository and run
+```
+python3 -m pip install -r requirements.txt
+```
+Additionnaly, install [wkhtmltopdf](https://wkhtmltopdf.org/index.html).  
+
+Usage:
+```
+% python3 -m esm_api_docs --url https://ESM_URL/rs/esm/v2/help
+```
+
---
+++ esm_api_docs/__main__.py
@@ -0,0 +1,31 @@
+
+import argparse
+import shlex
+import subprocess
+from .scrape import scrape
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="""Create a PDF of the McAfee SIEM API documentation. Store the PDF as "McAfee_SIEM_API_documentation.pdf".""", prog="python3 -m esm_api_docs")
+    parser.add_argument( '--url', help="API help URL", required=True )
+    parser.add_argument( '--out', help="Output PDF. Default is 'McAfee_SIEM_API_documentation.pdf'", default="McAfee_SIEM_API_documentation.pdf" )
+    parser.add_argument( '--wkhtmltopdf_args', help="wkhtmltopdf arguments as String")
+    args = parser.parse_args()
+    return(args)
+
+if __name__=="__main__":
+    args = parse_args()
+    print("Getting SIEM API ressources URLs list...")
+    items = scrape('esm_api_docs', spider_kwargs={'url':args.url}, additionnal_settings={'LOG_LEVEL':'ERROR'})
+    urls = [ i['url'] for i in items ]
+    summary = [ urls.pop(0) ]
+    methods = sorted([u for u in urls if '/help/types/' not in u])
+    types = sorted([u for u in urls if '/help/types/' in u])
+    wkhtmltopdf_cmd = ["wkhtmltopdf"]
+    if args.wkhtmltopdf_args:
+        wkhtmltopdf_cmd += shlex.split(args.wkhtmltopdf_args)
+    wkhtmltopdf_cmd += summary + methods + types + [args.out]
+    print("Generating PDF file...")
+    p = subprocess.run(args=wkhtmltopdf_cmd, timeout=300, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if p.returncode!=0:
+        raise(RuntimeError(p.stderr))
+    print("Done, the file {} has been generated. ".format(args.out))
